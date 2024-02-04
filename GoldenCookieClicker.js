88015f63e3c4dd235abaf5c9efb133a713b8fda3
Y.C.Huang <33322926+ShiriNmi1520@users.noreply.github.com>
2024-02-05 05:19:48 +0800

    Add files via upload

---
+++ GoldenCookieClicker.js
@@ -0,0 +1,50 @@
+// Golden Cookie Clicker
+// By RainSlide
+// Modified By ShiriNmi
+// Automatically click golden cookies and reindeers.
+// Version 1.0
+// Updated 2022-04-19
+
+// Cookie Clicker: https://orteil.dashnet.org/cookieclicker/
+
+"use strict";
+
+if (typeof Game !== "object" || Game === null || !Array.isArray(Game.shimmers)) {
+
+	console.error(
+		"Golden Cookie Clicker: Cookie Clicker API is not ready or invalid.\n" +
+		"Please make sure you are running this script on a Cookie Clicker webpage, " +
+		"and the page is fully loaded."
+	);
+
+} else if (typeof Proxy !== "function") {
+
+	console.error(
+		"Golden Cookie Clicker: JavaScript Proxy API is not available, " +
+		"either update your browser, or use the ES3 compatible version:\n"+
+		"https://rainslide.neocities.org/cookieclicker/GoldenCookieClicker.es3.js"
+	);
+
+} else {
+
+	const apply = (target, _this, args) => {
+		var shimmer = args[0];
+		console.debug('shimmer', shimmer)
+		if (shimmer.type === "golden" || shimmer.type === "reindeer") {
+			setTimeout(() => shimmer.pop(), 500);
+		}
+		return Reflect.apply(target, _this, args);
+	};
+
+	Object.defineProperty(Game.shimmers, "push", {
+		value: new Proxy(Game.shimmers.push, { apply }),
+		writable: true,
+		enumerable: false,
+		configurable: true
+	});
+
+	typeof Game.Win === "function" &&
+	Game.Win("Third-party");
+
+
+}
